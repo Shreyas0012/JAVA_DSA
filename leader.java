8e82ebc0c28586ae299b1e6bc4a7df0ba841b2a9
shreyas <shreyasupadhye494949@gmail.com>
2026-01-13 12:13:20 +0530

    initial commit

---
+++ leader.java
@@ -0,0 +1,17 @@
+public class leader{
+    static void leaders(int[]arr){
+        int max=arr[arr.length-1];
+        System.out.println(max);
+        for(int p=arr.length-2;p>=0;p--){
+            if(arr[p]>max){
+                max=arr[p];
+                System.out.println(max);
+            }
+        }
+  
+    }
+    public static void main(String[] args) {
+        int []arr={12,33,43,66,2,12,43};
+        leaders(arr);
+    }
+}
