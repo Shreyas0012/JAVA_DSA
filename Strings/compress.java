2e1f56dd26b6202b2b96d507a2564d9995de1e2b
Shreyas0012 <Shreyas0012>
2026-01-31 22:46:15 +0530

    refactor compress method for improved readability and consistency

--- Strings/compress.java
+++ Strings/compress.java
@@ -1,25 +1,24 @@
-public class compress {
+public class compress{
     static void compress(String str){
-       int count=1;
-       StringBuilder res =new StringBuilder();
-       for(int i=1;i<str.length();i++){
+    StringBuilder res =new StringBuilder();
+    int count=1;
+    for (int i=1;i<str.length();i++){
         if(str.charAt(i)==str.charAt(i-1)){
             count++;
         }
         else{
             res.append(str.charAt(i-1));
             res.append(count);
-            count=1;
+            count=1;  
         }
         if(i==str.length()-1){
             res.append(str.charAt(i));
             res.append(count);
         }
-       }
-       System.out.println(res); 
+    }System.out.println(res);
     }
     public static void main(String[] args) {
-        String str="aaabbccdee";
+        String str="aaasssttyou";
         compress(str);
     }
 }
