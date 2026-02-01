6593f83a6bcb27bc5081806cd89de08e9b25876e
Shreyas0012 <Shreyas0012>
2026-02-01 18:39:47 +0530

    implement longestnonrepeat method to count non-repeating characters

--- kpop.java
+++ kpop.java
@@ -1,16 +1,29 @@
- import java.util.Stack;
- public class kpop{
-        Stack<Integer> stack =new Stack<>();
-        static void greaterelem_stack(int[] arr){
-        int currentmax=0;
-        
-        for(int i=0;i<arr.length;i++){
-           int top=stack.peek(i);
-           if(arr[i]>top){
-            stack.pop
-           }
-        }
-    }
-    public static void main(String[] args) {
-    }
- }
+public class kpop{
+   static void longestnonrepeat(String str){
+       int count=1;
+       String res="";
+      for(int i=1;i<str.length();i++){
+         char ch=str.charAt(i);
+         if(ch==str.charAt(i-1)){
+           count++;
+         }
+         else{
+           res+=str.charAt(i-1);
+           res+=count;
+           count=1;
+         }
+         if(i==str.length()-1){
+            res+=ch;
+            res+=count;
+         }
+      }
+      System.out.println(res);
+   }
+          public static void main(String[] args) {
+      String str="AAA";
+      longestnonrepeat(str);      }
+}
+   
+
+
+
