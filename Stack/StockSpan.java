e523182b93b3fd4b968d348715af7ae3da67959e
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-03 16:13:35 +0530

    inital commit

--- Stack/StockSpan.java
+++ Stack/StockSpan.java
@@ -5,12 +5,15 @@ public class StockSpan {
         Stack<Integer> stack=new Stack<>();
         span[0]=1;
         for(int i=0;i<arr.length;i++){
+
             while(!stack.empty()&&arr[stack.peek()]<=arr[i]){
                 stack.pop();
             }
+
               if(stack.empty()){
                 span[i]=i+1;
               }
+              
               else{
                 span[i]=i-stack.peek();
               }
