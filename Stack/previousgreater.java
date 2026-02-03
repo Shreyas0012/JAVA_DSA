abc98184d4bbc5ad567c038ed45e5ab2191d36d3
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-03 16:50:00 +0530

    refactor StockSpan and previousgreater classes for improved readability and consistency

--- Stack/previousgreater.java
+++ Stack/previousgreater.java
@@ -7,20 +7,18 @@ public class previousgreater {
         Stack<Integer> stack =new Stack<>();
 
         for(int i=0;i<arr.length;i++){
-
-            while(!stack.empty()&&stack.peek()<=arr[i]){
-                 stack.pop();
+            while(!stack.isEmpty() && stack.peek()<=arr[i]){
+                stack.pop();
             }
-
             if(stack.isEmpty()){
-                System.out.println(arr[i]+"-> -1");
+                System.out.println(arr[i]+" -> "+-1);
             }
             else{
-                System.out.println(arr[i]+"->"+stack.peek());
+                System.out.println(arr[i]+" -> "+stack.peek());
             }
             stack.push(arr[i]);
         }
-    }
+        }
     public static void main(String[] args) {
         int[] arr = {100,20,80,90};
         previousGreater(arr);
