6ad4f47964ef4fd064ead2b57e7b97f8eb6cea8b
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-03 17:31:02 +0530

    initial commit

---
+++ Stack/Matchingbrackets.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class Matchingbrackets {
+    static boolean areBracketsBalanced(String str){
+       Stack<Character> stack=new Stack<>();
+       for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        if(ch=='('||ch=='{'||ch=='['){
+            stack.push(ch);
+        }
+        else{
+            if(stack.isEmpty()){
+                return false;
+            }
+        
+        char top=stack.pop();
+        if(ch==')' && top!='(') return false;
+        if(ch=='}' && top!='{') return false;
+        if(ch==']' && top!='[') return false;
+        }
+       }
+         return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str = "{[()]}";
+        System.out.println(areBracketsBalanced(str));
+    }
+}
