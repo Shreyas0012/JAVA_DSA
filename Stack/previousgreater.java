5d0a951049dc36dac8b4d35b12a066eed55ef92a
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-04 10:49:34 +0530

    inital commit

--- Stack/previousgreater.java
+++ Stack/previousgreater.java
@@ -3,23 +3,20 @@ import java.util.Stack;
 public class previousgreater {
 
     static void previousGreater(int[] arr) {
-     
-        Stack<Integer> stack =new Stack<>();
-
-        for(int i=0;i<arr.length;i++){
-            while(!stack.isEmpty() && stack.peek()<=arr[i]){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                System.out.println(arr[i]+" -> "+-1);
-            }
-            else{
-                System.out.println(arr[i]+" -> "+stack.peek());
-            }
-            stack.push(arr[i]);
-        }
-        }
-    public static void main(String[] args) {
+     Stack<Integer> stack=new Stack<>();
+     for(int i=0;i<arr.length;i++)
+{      while(!stack.empty() && stack.peek()<=arr[i]){
+    stack.pop();
+}
+         if(stack.isEmpty()){
+            System.out.println(arr[i]+"->"+-1);
+         }
+         else{
+            System.out.println(arr[i]+"->"+stack.peek());
+         }
+         stack.push(arr[i]);
+}        }
+            public static void main(String[] args) {
         int[] arr = {100,20,80,90};
         previousGreater(arr);
     }
