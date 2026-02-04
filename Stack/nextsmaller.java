84ebde03da80fbf8cdb85263856482b1a148e810
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-04 21:08:50 +0530

    initial commit

---
+++ Stack/nextsmaller.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class nextsmaller {
+
+    static void previousGreater(int[] arr) {
+     Stack<Integer> stack=new Stack<>();
+     for(int i=arr.length-1;i>=0;i--)
+{      while(!stack.empty() && stack.peek()>=arr[i]){
+        stack.pop();
+         }
+         if(stack.isEmpty()){
+            System.out.println(arr[i]+"->"+-1);
+         }
+         else{
+            System.out.println(arr[i]+"->"+stack.peek());
+         }
+         stack.push(arr[i]);
+}        }
+            public static void main(String[] args) {
+        int[] arr = {100,20,80,30,20,80};
+        previousGreater(arr);
+    }
+}
