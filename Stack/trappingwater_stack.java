aebe0bb03043552ced6d6cb8703f4d6a275f879c
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-08 20:53:33 +0530

    initial commit

---
+++ Stack/trappingwater_stack.java
@@ -0,0 +1,15 @@
+public class trappingwater_stack {
+    static int search(int[]arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int target=7;
+        int[]arr={1,4,5,7,82};
+        System.out.println(search(arr,target));
+    }
+}
