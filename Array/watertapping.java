88556ae218f62607eb9646802d4be589df38c6af
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-09 23:13:42 +0530

    refactoring linear search and watertrapping problem

--- Array/watertapping.java
+++ Array/watertapping.java
@@ -1,34 +1,27 @@
-package Array;
+                 
 public class watertapping{
 static int bars(int[] arr) {
-    int left = 0;
-    int right = arr.length - 1;
-    int leftMax = 0, rightMax = 0;
+    int l = 0;
+    int r = arr.length - 1;
+    int leftmax = 0, rightmax = 0;
     int water = 0;
-  while(left<right){
-    if(arr[left]<arr[right]){
-        if(arr[left]>leftMax){
-           leftMax=arr[left];
-        }
-        else{
-            water+=leftMax-arr[left];
-        }
-        left++;
-    }
-    else{
-        if(arr[right]>rightMax){
-            rightMax=arr[right];
-        }
-        else{
-            water+=rightMax-arr[right];
-        }
-        right--;
-    }
-  }
+  while(l<r){
+     leftmax=Math.max(leftmax,arr[l]);
+     rightmax=Math.max(rightmax,arr[r]);
+     if(leftmax>rightmax){
+        water+=rightmax-arr[r];
+        r--;
+     }else{
+        water+=leftmax-arr[l];
+        l++;
+     }
+     }
+
+  
     return water;
-}
+    }
 public static void main(String[] args) {
-    int [] arr={3,0,1,3};
+    int [] arr={6,0,1,4};
     System.out.println(bars(arr));
 }
 }
