af5455dc8e227e2d217db45e7b699dd04ee8e416
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-10 23:52:27 +0530

    refactoring initial commit

--- quicksort.java
+++ quicksort.java
@@ -1,29 +1,25 @@
 public class quicksort {
 
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-
-            int p = partition(arr, low, high);
-
-            quickSort(arr, low, p - 1);   // left of pivot
-            quickSort(arr, p + 1, high);  // right of pivot
+    static void quicksort(int[] arr, int low, int high) {
+        if(low<high){
+            int p=partition(arr,low,high);
+            quicksort(arr,low,p-1);
+            quicksort(arr,p+1,high);
         }
     }
-
     static int partition(int[] arr, int low, int high) {
-
-        int pivot = arr[high];  // choose last element
-        int i = low - 1;        // index of smaller element
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<=pivot){
                 i++;
-                swap(arr, i, j);
+                swap(arr,i,j);
             }
         }
-
-        swap(arr, i + 1, high);
-        return i + 1;   // pivot index
+            swap(arr,i+1,high);
+        
+            return i+1;
+        
     }
 
     static void swap(int[] arr, int i, int j) {
@@ -34,7 +30,7 @@ public class quicksort {
 
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1};
-        quickSort(arr, 0, arr.length - 1);
+        quicksort(arr, 0, arr.length - 1);
 
         for (int x : arr) {
             System.out.print(x + " ");
