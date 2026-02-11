ffdee9fd57bc8cf55cca7c2875c4934fb12464cf
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-11 23:39:28 +0530

    inital commit of hashmap example

---
+++ duplicate_hashmap.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class duplicate_hashmap {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,2,4,4};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : arr){
+            if(map.containsKey(num)){
+                System.out.println("Duplicate found: " + num);
+                
+                return;
+            
+            }
+            map.put(num,1);
+            
+        }
+
+        System.out.println("No duplicate");
+    }
+}
