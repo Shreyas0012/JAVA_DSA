0523abb6c7d2ff6240b290f794f18db6d48fdf49
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-14 21:47:06 +0530

    initial commit

---
+++ appearonce.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+
+public class appearonce{
+    static void find(int[]arr){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:arr){
+            if(map.get(num)==1)
+            System.out.println(num);
+        }
+    }
+    public static void main(String[] args) {
+
+        int []arr={1,2,1,2,3,4};
+        find(arr);
+    }
+}
