f01e49eb5456d0a73c525b1be0bd32a8648318d6
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-20 17:45:11 +0530

    initial commit of mixed question on binary search

---
+++ searchinrotated_binary.java
@@ -0,0 +1,36 @@
+public class searchinrotated_binary {
+    static int search(int[]arr,int k){
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            if(arr[mid]>=arr[low]){
+                if(k>=arr[low] && k<arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(k>arr[mid] && k<=arr[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int result = search(arr, target);
+        System.out.println(result);
+    }
+}
