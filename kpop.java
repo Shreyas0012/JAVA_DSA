d2fc8990f7bb5433ca188c361a91587a43eaa46f
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-22 23:22:43 +0530

    initial commit

--- kpop.java
+++ kpop.java
@@ -1,35 +1,29 @@
-public class kpop {
-    static int search(int[]arr,int k){
-        int low=0;
-        int high=arr.length-1;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-         if(arr[mid]==k){
-                return mid;
-            }
-        if(arr[low]<=arr[mid]){
-            if(k>=arr[low] && k<arr[mid]){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+public class kpop{
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-            else{
-                 if(arr[mid]<=k && arr[high]>k){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-            }
-       }
-        return -1;
     }
+
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
-        int result = search(arr, target);
-        System.out.println(result);
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+
+        a.next = b;
+        b.next = c;
+
+        Node temp = a;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
     }
 }
