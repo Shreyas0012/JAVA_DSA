5b9e0a84ea952b0060b01a68a487b5f747cb2d7e
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-26 22:57:48 +0530

    finding longest consecutive number using hashmap

---
+++ consecutive_hashmap.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+
+public class consecutive_hashmap {
+
+    static int longest(int[] arr) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int max = 0;
+
+        for (int num : set) {
+
+            // only start if it's beginning of sequence
+            if (!set.contains(num - 1)) {
+
+                int current = num;
+                int count = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                max = Math.max(max, count);
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println(longest(arr));
+    }
+}
