d25506adcfe468891082a3211c578efdff0e281b
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-02-26 23:24:55 +0530

    simple task sceduler problem

---
+++ tasksceduler.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+
+public class tasksceduler {
+
+    static int leastInterval(char[] tasks, int n) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+
+        int maxFreq = 0;
+
+        // Find max frequency
+        for (int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        int maxCount = 0;
+
+        // Count how many tasks have max frequency
+        for (int freq : map.values()) {
+            if (freq == maxFreq) {
+                maxCount++;
+            }
+        }
+
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int emptySlotsFormula = partCount * partLength + maxCount;
+
+        return Math.max(tasks.length, emptySlotsFormula);
+    }
+
+    public static void main(String[] args) {
+
+        char[] tasks = {'A','A','A','B','B','B'};
+        int n = 2;
+
+        System.out.println(leastInterval(tasks, n));
+    }
+}
